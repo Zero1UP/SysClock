63ac24350961a4aa49d6d1e7691c8ac551d3db9a
Zero1UP <Zero1UP@gmail.com>
2015-03-11 22:30:07 -0400

    First Commit
    Initial Commit

---
+++ SysClock/frm_Sysclock.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SysClock
+{
+    partial class frm_Sysclock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cms_RightClick = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmr_ResetForm = new System.Windows.Forms.Timer(this.components);
+            this.tmr_UpdateTime = new System.Windows.Forms.Timer(this.components);
+            this.cms_RightClick.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cms_RightClick
+            // 
+            this.cms_RightClick.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.cms_RightClick.Name = "cms_RightClick";
+            this.cms_RightClick.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // tmr_ResetForm
+            // 
+            this.tmr_ResetForm.Enabled = true;
+            this.tmr_ResetForm.Interval = 1000;
+            this.tmr_ResetForm.Tick += new System.EventHandler(this.tmr_ResetForm_Tick);
+            // 
+            // tmr_UpdateTime
+            // 
+            this.tmr_UpdateTime.Enabled = true;
+            this.tmr_UpdateTime.Interval = 1000;
+            this.tmr_UpdateTime.Tick += new System.EventHandler(this.tmr_UpdateTime_Tick);
+            // 
+            // frm_Sysclock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "frm_Sysclock";
+            this.Text = "Form1";
+            this.cms_RightClick.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip cms_RightClick;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Timer tmr_ResetForm;
+        private System.Windows.Forms.Timer tmr_UpdateTime;
+
+    }
+}
+
---
+++ SysClock/frm_Sysclock.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing;
+namespace SysClock
+{
+    public partial class frm_Sysclock : Form
+    {
+        // Windows API calls
+        //public const int WM_NCLBUTTONDOWN = 0xA1;
+        //public const int HT_CAPTION = 0x2;
+        //[DllImportAttribute("user32.dll")]
+        //public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        //[DllImportAttribute("user32.dll")]
+        //public static extern bool ReleaseCapture();
+
+        public frm_Sysclock()
+        {
+            InitializeComponent();
+
+            //Set the form so it is topmost
+            this.TopMost = true;
+
+            //Set some form properties so it doesn't look like a form
+            this.ControlBox = false;
+            this.Text = "";
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            //Set the form size
+            this.Width = 75;
+            this.Height = 60;
+
+            //Set the form color
+            this.BackColor = Color.CadetBlue;
+            this.Font = new Font(this.Font, FontStyle.Bold);
+
+
+
+            //Set the form to the bottom right of the screen
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width, workingArea.Bottom - Size.Height);
+
+            //Add Labels
+            CreateLabels();
+
+            //Add contextMenuStrip
+            this.ContextMenuStrip = cms_RightClick;
+        }
+
+       
+       //Un-used code at the time being. This would be used to allow the use to drag the window by clicking anywhere on the form 
+        //private void frm_Sysclock_MouseDown(object sender, MouseEventArgs e)
+        //{
+        //    ReleaseCapture();
+        //    SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0); 
+        //}
+
+
+        private void CreateLabels()
+        {
+
+            //Create Label objects
+            Label dayLabel = new Label
+            {
+                Name = "lbl_dayLabel",
+                BackColor = Color.Transparent,
+                ForeColor = Color.White,
+                Location = new Point(30,0),
+                Text = DateTime.Now.DayOfWeek.ToString(),
+                Font = new Font(this.Font.FontFamily, 10)
+            };
+
+
+            Label timeLabel = new Label
+            {
+                Name = "lbl_timeLabel",
+                BackColor = Color.Transparent,
+                ForeColor = Color.White,
+                Location = new Point(30, 20),
+                Text =DateTime.Now.ToString("hh:mm:ss tt"),
+                Font = new Font(this.Font.FontFamily, 10)
+            };
+
+            Label  dateLabel = new Label
+            {
+                Name = "lbl_dateLabel",
+                BackColor = Color.Transparent,
+                ForeColor = Color.White,
+                Location = new Point(30, 40),
+                Text =DateTime.Now.ToString(),
+                Font = new Font(this.Font.FontFamily, 10)
+            };
+
+            //Add Labels to the form
+            this.Controls.Add(dayLabel);
+            this.Controls.Add(timeLabel);
+            this.Controls.Add(dateLabel);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tmr_ResetForm_Tick(object sender, EventArgs e)
+        {
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width, workingArea.Bottom - Size.Height);
+        }
+
+        private void tmr_UpdateTime_Tick(object sender, EventArgs e)
+        {
+           foreach (Control Label in this.Controls)
+           {
+               if (Label.Name == "lbl_timeLabel")
+                   Label.Text = DateTime.Now.ToString("hh:mm:ss tt");
+
+           }
+        }
+
+
+    }
+}
